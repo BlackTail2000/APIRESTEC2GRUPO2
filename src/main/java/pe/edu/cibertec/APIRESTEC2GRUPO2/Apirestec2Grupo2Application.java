a18182296f42f1bcfac925f5aeb0883e5397a12d
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-04-26 10:17:25 -0500

    application.properties completado

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2GRUPO2/Apirestec2Grupo2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2GRUPO2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestec2Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestec2Grupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/APIRESTEC2GRUPO2/Apirestec2Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2GRUPO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestec2Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
